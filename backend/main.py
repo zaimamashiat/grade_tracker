626cbb4b28eaba0927e1a3ebe74be712bf216f07
Zaima Mashiat <153401737+zaimamashiat@users.noreply.github.com>
2026-04-19 21:30:56 +0600

    first commit

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-DB_PATH = "gradeflow.db"
+DB_PATH = "backend/gradeflow.db"
 
 
 # ── DB Init ───────────────────────────────────────────────────────────────────
