6759b1dc8e045157cf8ced0542d6de9b146545a6
Zaima Mashiat <153401737+zaimamashiat@users.noreply.github.com>
2026-04-19 21:22:21 +0600

    first commit

--- frontend/app.py
+++ frontend/app.py
@@ -2,9 +2,8 @@ import streamlit as st
 import plotly.graph_objects as go
 from datetime import date
 import requests
-
-# ── Config ────────────────────────────────────────────────────────────────────
-API = "http://127.0.0.1:8000"
+import os
+API = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 
 # ── Page Config ───────────────────────────────────────────────────────────────
 st.set_page_config(
